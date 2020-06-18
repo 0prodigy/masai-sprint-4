00a7d599cc5c6a55af6b6b1d676bd2fb95a17c26
Akash <pathakvikash9211@gmail.com>
2020-06-18 10:22:58 +0530

    added generate quotation and add produclist function

--- main.js
+++ main.js
@@ -65,3 +65,55 @@ function debounce(func, delay) {
     timer = setTimeout(func, delay);
   };
 }
+
+//generate quotation
+var quotationArr = [];
+
+function generateQuotation() {
+  event.preventDefault();
+  var customerName = document.getElementById("customerName").value;
+  var customerAddress = document.getElementById("address").value;
+  var companyName = document.getElementById("company").value;
+  var compnayAdress = document.getElementById("companyAddress").value;
+  var products = document.querySelectorAll(".productName");
+  var quantity = document.querySelectorAll(".quantity");
+  var desc = document.querySelectorAll(".description");
+
+  var productList = [];
+  for (var i = 0; i < products.length; i++) {
+    productList.push(products[i].value);
+  }
+
+  var quantities = [];
+  for (var i = 0; i < quantity.length; i++) {
+    quantities.push(quantity[i].value);
+  }
+
+  var descList = [];
+  for (var i = 0; i < desc.length; i++) {
+    descList.push(desc[i].value);
+  }
+
+  var quotation = {
+    customerName: customerName,
+    customerAddress: customerAddress,
+    companyName: companyName,
+    companyAddress: compnayAdress,
+    productList: productList,
+    quantities: quantities,
+    descList: descList,
+  };
+
+  quotationArr.push(quotation);
+}
+
+// create product list html layout
+var productListBtn = document.getElementById("addProductList");
+productListBtn.addEventListener("click", addProductList);
+
+function addProductList() {
+  var productsList = document.querySelector(".productsList");
+  var list = document.createElement("div");
+  list.innerHTML = productsList.innerHTML;
+  productsList.append(list);
+}
